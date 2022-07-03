e779e56b1b847f7152aaa35f27d61fa87ccfbb16
jinseop <yeomjinseop@gmail.com>
2022-07-03 17:52:21 +0900

    Chapter3

---
+++ Chapter3/ProfileHandler.java
@@ -0,0 +1,9 @@
+public class ProfileHandler {
+  public static void main(String[] args) {
+    Profile person1 = new Profile();
+    person1.setName("Min");
+    person1.setAge(20);
+    person1.printName();
+    person1.printAge();
+  }
+}
