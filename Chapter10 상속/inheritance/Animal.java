f4c70bc70db632a704453f5cf07e676d186c9034
jinseop <yeomjinseop@gmail.com>
2022-07-05 15:04:34 +0900

    Chapter10

---
+++ Chapter10 상속/inheritance/Animal.java
@@ -0,0 +1,13 @@
+package c.inheritance;
+
+public class Animal {
+  String name;
+  String kind;
+  int legCount;
+
+  public void move(){
+    System.out.println("Animal - moves")
+  }
+
+  public void eatFood(){}
+}
---
+++ Chapter10 상속/inheritance/Dog.java
@@ -0,0 +1,11 @@
+package c.inheritance;
+
+public class Dog extends Animal {
+
+  boolean isTrained;
+
+  public void move(){
+    System.out.println("Dog - moves")
+  }
+  
+}
